5170800a8c88ae69c2d7a4771e1378a60d6d9e24
sota1235 <sota1235@gmail.com>
2016-05-13 11:57:14 +0900

    modify: parse files of argument

--- src/file-size-checker.js
+++ src/file-size-checker.js
@@ -8,10 +8,18 @@
 "use strict";
 
 import { create } from 'node-getopt';
+import glob from 'glob';
 
+// get arguments
 const opt = create([
   ['', 'files=ARG', 'target files for lint'],
   ['', 'limit=ARG', 'limit size'],
 ]).bindHelp().parseSystem();
 
 console.log(opt);
+
+const targetFiles = opt.options.files;
+
+glob(targetFiles, (err, files) => {
+  console.log(files);
+});
