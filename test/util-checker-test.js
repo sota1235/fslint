81a971b0eee82428eb31d638cb182f733d3b6b38
sota1235 <sota1235@gmail.com>
2016-05-17 23:09:59 +0900

    modify: add unit test for src/util/checker.js

---
+++ test/util-checker-test.js
@@ -0,0 +1,36 @@
+/**
+ * @description Unit test for util/checker
+ */
+
+import 'babel-polyfill';
+import assert     from 'power-assert';
+import proxyquire from 'proxyquire';
+
+describe('Unit tests for util/checker', () => {
+  it('Should resolve true', async () => {
+    const mockMethod = proxyquire('../src/util/checker', {
+      './fs-promise': {
+        stat(file) {
+          return new Promise((resolve) => resolve({ size: 100 }));
+        },
+      },
+    });
+
+    assert(await mockMethod.default('file path', 100));
+    assert(await mockMethod.default('file path', 101));
+  });
+
+  it('Should resolve false', async () => {
+    const mockMethod = proxyquire('../src/util/checker', {
+      './fs-promise': {
+        stat(file) {
+          return new Promise((resolve) => resolve({ size: 100 }));
+        },
+      },
+    });
+
+    assert(!(await mockMethod.default('file path', 99)));
+    assert(!(await mockMethod.default('file path', 50)));
+  });
+
+});
