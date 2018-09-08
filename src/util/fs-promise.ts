0bb73ffe1911cf2080b2780163f27a1e59014594
sota1235 <sota1235@gmail.com>
2018-09-08 18:10:53 +0900

    modify: rewrite to ts

---
+++ src/util/fs-promise.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+import {Stats} from 'fs';
+
+export function stat(filePath: string): Promise<Stats> {
+  return new Promise((resolve, reject) => {
+    fs.stat(filePath, (err: NodeJS.ErrnoException, stats: Stats) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(stats);
+    });
+  });
+}
